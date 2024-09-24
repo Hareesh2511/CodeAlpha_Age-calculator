ce253a7277b1635d2b33b7b847dd2fb899582c21
Hareesh Chintada <chintadahareesh10@gmail.com>
2024-09-24 12:23:03 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,21 @@
+function calculateAge() {
+    const dobInput = document.getElementById('dob').value;
+    if (!dobInput) {
+        alert("Please select your date of birth.");
+        return;
+    }
+
+    const dob = new Date(dobInput);
+    const today = new Date();
+
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+    const dayDiff = today.getDate() - dob.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+
+    document.getElementById('result').innerText = `Your age is: ${age} years`;
+	
+}
